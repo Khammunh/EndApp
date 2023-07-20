b4c1b0964567ee2071306149d1cd3b3619cc1e52
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-07-20 23:13:48 +0700

    create AddPlaceScreen

---
+++ lib/screens/add_place.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  const AddPlaceScreen({super.key});
+
+  @override
+  State<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text('Add Place Screen'),
+      ),
+    );
+  }
+}
